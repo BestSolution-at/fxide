ac5b327c5483e35670024234ff9cb1cde2d126c4
Tom Schindl <tom.schindl@bestsolution.at>
2017-04-04 22:34:23 +0200

    Various changes
    * loading stylesheets
    * fixed copyright

---
+++ at.bestsolution.fxide.app/src/at/bestsolution/fxide/app/PatternflySaveDialogPresenterTypeProvider.java
@@ -0,0 +1,27 @@
+//package at.bestsolution.fxide.app;
+//
+//import java.util.List;
+//
+//import org.eclipse.e4.ui.workbench.modeling.ISaveHandler.Save;
+//import org.eclipse.fx.ui.workbench.renderers.fx.services.SaveDialogPresenter;
+//import org.eclipse.fx.ui.workbench.renderers.fx.services.SaveDialogPresenterTypeProvider;
+//import org.osgi.service.component.annotations.Component;
+//
+//@Component
+//public class PatternflySaveDialogPresenterTypeProvider implements SaveDialogPresenterTypeProvider {
+//
+//	@Override
+//	public <S extends SaveDialogPresenter> Class<S> getType() {
+//		return null;
+//	}
+//
+//	public static class SaveDialogPresenterImpl implements SaveDialogPresenter {
+//
+//		@Override
+//		public List<Save> promptToSave(SaveData data) {
+//			// TODO Auto-generated method stub
+//			return null;
+//		}
+//
+//	}
+//}
